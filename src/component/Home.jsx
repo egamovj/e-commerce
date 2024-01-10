75b5de347a6d4abc500ce93a7eae5d0ab3420db4
Jo'rabek Egamov <134671078+egamovj@users.noreply.github.com>
2024-01-10 22:39:44 +0500

    navbar completed

---
+++ src/component/Home.jsx
@@ -0,0 +1,21 @@
+const Home = () => {
+  return (
+    <div className="hero">
+      <div className="card bg-dark text-white">
+        <img src="" className="card-img" alt="..." />
+        <div className="card-img-overlay">
+          <h5 className="card-title">Card title</h5>
+          <p className="card-text">
+            This is a wider card with supporting text below as a natural lead-in
+            to additional content. This content is a little bit longer.
+          </p>
+          <p className="card-text">
+            <small>Last updated 3 mins ago</small>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
